459fc7e100fb2945da223adcd92f94e0cd6d91b5
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-04-23 11:50:55 -0300

    criando rotas

---
+++ src/Pages/Detalhes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+    Container,
+} from './styles';
+
+export default function Detalhes() {
+    return (
+        <Container>
+            <h1>Bem-vindo, essa é a página DETALHES!!</h1>
+        </Container>
+    );
+}
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  Container,
+} from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Bem-vindo, essa é a página HOME!!</h1>
+    </Container>
+  );
+}
