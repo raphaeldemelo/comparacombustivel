4b03887ba20ba2a6d766e785c7e075c4e34ad0e4
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-04-24 10:10:37 -0300

    estilizando com styled-components

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import {
   Container,
+  Imagem,
+  Titulo,
+  Label,
+  Input,
+  Botao,
+  TextoBotao,
 } from './styles';
+import Logo from '../../assets/logo.png'
 
 export default function Home() {
   return (
     <Container>
-      <h1>Bem-vindo, essa é a página HOME!!</h1>
+      <Imagem src={Logo} />
+      <Titulo>Qual é a melhor opção?</Titulo>
+      <Label>Álcool (Preço por litro):</Label>
+      <Input />
+      <Label>Gasolina (Preço por litro):</Label>
+      <Input />
+      <Botao>
+        <TextoBotao>Calcular</TextoBotao>
+      </Botao>
     </Container>
   );
 }
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -1,3 +1,44 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100%;
+width: 100%;
+flex-direction: column;
+`;
+
+export const Imagem = styled.img`
+`;
+
+export const Titulo = styled.h1`
+
+`;
+export const Label = styled.strong`
+
+:nth-of-type(2) {
+margin-top: 20px;
+}
+`;
+export const Input = styled.input`
+border-radius: 8px;
+border: none;
+height: 30px;
+padding: 5px;
+margin-top: 3px;
+width: 30%;
+`;
+export const Botao = styled.button`
+padding: 10px;
+border: none;
+border-radius: 8px;
+background-color: #202020;
+cursor: pointer;
+margin-top: 20px;
+width: 30%;
+`;
+export const TextoBotao = styled.span`
+color: #fff;
+font-weight: bold;
+`;
